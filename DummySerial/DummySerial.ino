3debdb76936953640f3a4a393c72db10b4d6634e
An7orAhmed <antor.mee@gmail.com>
2023-11-27 14:58:57 +0600

    updated

--- DummySerial/DummySerial.ino
+++ DummySerial/DummySerial.ino
@@ -1,11 +1,15 @@
 
+int x = 0;
+
 void setup() {
   Serial.begin(115200);
   randomSeed(analogRead(34));  // I used ESP32, A0 for arduino boards
 }
 
 void loop() {
-  float volt = random(0, 500) / 100.0;
+  float volt = x / 100.0;
   Serial.println(volt);
   delay(2);
+  x++;
+  if(x >= 500) x = 0; 
 }
